2d7edad79b548ccf4fdcebfceef9ef35b867f33e
Edijs Endijs Eglītis <110787421+EdijsEndijsEglitis@users.noreply.github.com>
2023-05-10 09:35:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const coolButton = document.querySelector(".js-cool");
+
+coolButton.addEventListener("mouseover", () => {
+  window.open(
+    "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+  );
+});
